4b559b371d87bafe8b976286e3f80685f97efdfb
Augustine von Trapp <augustvontrapp@gmail.com>
2018-01-16 22:52:40 -0800

    made first legit passing saga test

--- src/duck/operations/__tests__/sagas.test.js
+++ src/duck/operations/__tests__/sagas.test.js
@@ -15,9 +15,9 @@ const func = ()=>({});
 
 it('fetchUser', () => {
   const gen = fetchUser();
-  console.log(deepEqual( gen.next().value, call(api, {key: 'whu', params: 'who'})));
+  // console.log(deepEqual( gen.next().value, call(api, {key: 'whu', params: 'who'})));
   // console.log(gen.next().value, call(api('fake_data'), {txnId: '3'}));
-  console.log(deepEqual( call(func, {id: 3}), call(func, {id: 3}) ));
+  // console.log(deepEqual( call(func, {id: 3}), call(func, {id: 3}) ));
   // console.log(call(api('fake_data')));
-  // expect( call(api('foo'), {h: 3}) ).toEqual( call(api('foo'), {h: 3}) )
+  expect( gen.next().value ).toEqual( call(api, {key: 'whu', params: 'who'}) );
 });
